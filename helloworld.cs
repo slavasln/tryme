858260bd0f96ae8ed57a6db5c978c6fc2f3aa873
Stanislav Natkovskyi <sln@gmx.net>
2016-03-19 20:36:02 +0200

    Wrote 1st program in C#

---
+++ helloworld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace tryme 
+{
+    public class Program 
+    {
+        public static Main() 
+        {
+            Console.WriteLine("Hello World");
+        } 
+    }
+} 
